5f06294f24329398ce01d922db6b32ce378773de
clark1816 <clark.mcquiston@gmail.com>
2022-09-09 12:16:29 -0400

    added loginpage

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
+
+from unicodedata import name
 from urllib import request
-from fastapi import FastAPI, Request, Form, File
+from fastapi import FastAPI, Request, Form, File, UploadFile
+from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import sqlite3, config
 import alpaca_trade_api as api
 from alpaca_trade_api.rest import REST, TimeFrame, TimeFrameUnit
 import psycopg2
 import psycopg2.extras
+from flask import Flask, request, render_template
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.post("/add")
 async def index(request: Request, add_symbol: str = Form(...), add_exchange: str = Form(...)):
@@ -67,36 +74,40 @@ async def index(request: Request, delete_stock: str = Form(...)):
     order_symbols = [order.symbol for order in orders if x == 1]
 
     return templates.TemplateResponse("index.html", {"request": request, "stocks": rows, "orders": order_symbols})
-@app.get("/")
-async def index(request: Request):
+
+@app.get("/", response_class=HTMLResponse)
+def index(request: Request):
     return templates.TemplateResponse("login.html", {"request": request})
-database={'letstrade':'123'}
+db_username = 'letstrade'
+db_password = '123'
 
+@app.post('/', response_class=HTMLResponse)
+async def index(request: Request, username: str = Form(...), password: str = Form(...)):
+    print(type(username))
+    print(f'password: {password}')
+    if username == db_username:
+        if password == db_password:
+            print("success")
+            connection = psycopg2.connect(port = config.DB_PORT,host=config.DB_HOST, database=config.DB_NAME, user=config.DB_USER, password=config.DB_PASS)
+            cursor = connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
+            cursor.execute("""
+                SELECT symbol from watchlist 
+            """)
+            rows = cursor.fetchall()
 
+            #get current positions 
+            tradeing_client = api.REST(config.API_Key, config.API_Secret, config.URL_ENDPT)
+            BASE_URL = config.URL_ENDPT
+            print(BASE_URL)
+            x = 1
+            orders = tradeing_client.list_positions()  
+            #for order in orders:
+                # print(order.symbol)
+                # print(f"Already a trade open for: {order.symbol} skipping")
+            #list comprhension is not easy
+            order_symbols = [order.symbol for order in orders if x == 1]
 
-@app.route('/form_login',methods=['POST','GET'])
-@app.get("/")
-async def index(request: Request):
-    connection = psycopg2.connect(port = config.DB_PORT,host=config.DB_HOST, database=config.DB_NAME, user=config.DB_USER, password=config.DB_PASS)
-    cursor = connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    cursor.execute("""
-        SELECT symbol from watchlist 
-    """)
-    rows = cursor.fetchall()
-    
-    #get current positions 
-    tradeing_client = api.REST(config.API_Key, config.API_Secret, config.URL_ENDPT)
-    BASE_URL = config.URL_ENDPT
-    print(BASE_URL)
-    x = 1
-    orders = tradeing_client.list_positions()  
-    #for order in orders:
-        # print(order.symbol)
-        # print(f"Already a trade open for: {order.symbol} skipping")
-    #list comprhension is not easy
-    order_symbols = [order.symbol for order in orders if x == 1]
-
-    return templates.TemplateResponse("index.html", {"request": request, "stocks": rows, "orders": order_symbols})
+            return templates.TemplateResponse("index.html", {"request": request, "stocks": rows, "orders": order_symbols})
     # return {"Title": "Dashboard", "stocks": rows}
 
 @app.get("/stock/{symbol}")
