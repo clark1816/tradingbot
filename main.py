5555fd56a5809972f7ebf815683690cbd7c92be6
clark1816 <33646776+clark1816@users.noreply.github.com>
2022-09-17 13:51:43 -0400

    Update main.py

--- main.py
+++ main.py
@@ -170,9 +170,45 @@ async def index(request: Request, symbol: str = Form(...)):
     cerebro.adddata(feed)
     cerebro.addstrategy(MyStrategy)
     cerebro.run()
-    cerebro.plot()
+    def saveplots(cerebro, numfigs=1, iplot=True, start=None, end=None,
+             width=16, height=9, dpi=300, tight=True, use=None, file_path = '', **kwargs):
+
+        from backtrader import plot
+        if cerebro.p.oldsync:
+            plotter = plot.Plot_OldSync(**kwargs)
+        else:
+            plotter = plot.Plot(**kwargs)
+
+        figs = []
+        for stratlist in cerebro.runstrats:
+            for si, strat in enumerate(stratlist):
+                rfig = plotter.plot(strat, figid=si * 100,
+                                    numfigs=numfigs, iplot=iplot,
+                                    start=start, end=end, use=use)
+                figs.append(rfig)
+
+        for fig in figs:
+            for f in fig:
+                f.savefig(file_path, bbox_inches='tight')
+        return figs
+
+    saveplots(cerebro, file_path = 'static/savefig.jpg')
 
     return templates.TemplateResponse("backtest.html", {"request": request, "symbol":symbol})
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+#print("Code Completed")
